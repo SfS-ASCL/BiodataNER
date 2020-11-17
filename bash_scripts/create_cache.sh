a84aca7108bd2fb8214149973294ab77ec7cf1ed
Blubberli <neele.witte@student.uni-tuebingen.de>
2020-11-17 14:16:43 +0100

    update script

--- bash_scripts/create_cache.sh
+++ bash_scripts/create_cache.sh
@@ -2,9 +2,9 @@
 path_to_cache="/Users/nwitte/PycharmProjects/biodataNER/cmdis/caches/persons.csv"
 cmdi_files="/Users/nwitte/PycharmProjects/biodataNER/cmdis/data"
 namespace="http://www.clarin.eu/cmd/1/profiles/clarin.eu:cr1:p_1527668176124"
-entity_tag="Contact"
+entity_tag="Person"
 authoritative_tag="AuthoritativeID"
 specification="/Users/nwitte/PycharmProjects/biodataNER/cmdis/entity_spec"
 entity_type="Personal"
 
-python3 cmdi_extractor.py $path_to_cache $cmdi_files $namespace $entity_tag $authoritative_tag $specification $entity_type
+python3 cmdi_extractor.py $path_to_cache $cmdi_files $namespace $entity_tag $authoritative_tag $specification $entity_type --new_cache
