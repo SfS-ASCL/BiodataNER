45c570c023d53acc49ee912f30a3098e9e03cec0
Nino Meisinger <nino.meisinger@student.uni-tuebingen.de>
2020-12-14 00:31:57 +0100

    modified with new argument structure

--- bash_scripts/update_cmdis.sh
+++ bash_scripts/update_cmdis.sh
@@ -1,10 +1,9 @@
 #!/usr/bin/env bash
-path_to_cache="/Users/nwitte/PycharmProjects/biodataNER/cmdis/caches/persons.csv"
-cmdi_files="cmdis/data"
-namespace="http://www.clarin.eu/cmd/1/profiles/clarin.eu:cr1:p_1527668176124"
-entity_tag="Person"
+path_to_cache="cache2.csv"
+cmdi_files="data/"
 authoritative_tag="AuthoritativeID"
-specification="/Users/nwitte/PycharmProjects/biodataNER/cmdis/entity_spec"
-new_cmdis="/Users/nwitte/PycharmProjects/biodataNER/cmdis/updated_cmdis"
+specification="entity_spec.json"
+namespace_tag_list="namespaces_tags.csv"
+new_cmdis="updated_cmdis"
 
-python3 update_cmdi.py $path_to_cache $cmdi_files $namespace $entity_tag $authoritative_tag $specification $new_cmdis --add_candidates
+python3 python_scripts/update_cmdi.py $path_to_cache $cmdi_files $specification $namespace_tag_list $authoritative_tag $new_cmdis
