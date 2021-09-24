fa22d761da52c24188eb137b1df99819e2732978
Kerl <nmeisinger@protonmail.com>
2021-09-24 12:32:43 +0200

    LF line endings

--- bash_scripts/create_and_update.sh
+++ bash_scripts/create_and_update.sh
@@ -6,7 +6,7 @@ specification="entity_spec.json"
 namespace_tag_list="namespaces_tags.csv"
 new_cmdis="updated_cmdis"
 
-python3 python_scripts/cmdi_extractor.py $path_to_cache $cmdi_files $authoritative_tag $specification $namespace_tag_list $1 #--new_cache
+python3 python_scripts/cmdi_extractor.py $path_to_cache $cmdi_files $authoritative_tag $specification $namespace_tag_list $1 --new_cache
 echo "cache created"
 echo "begin updating CMDIs"
 python3 python_scripts/update_cmdi.py $path_to_cache $cmdi_files $specification $namespace_tag_list $authoritative_tag $new_cmdis
